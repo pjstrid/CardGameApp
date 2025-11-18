769c532877794302dee9f0d58c4007488388494c
jonathan strid <pjstrid@outlook.com>
2025-11-18 19:00:51 +0000

    added pictures and set up layout draft

--- app/src/main/java/com/example/cardgameapp/GameActivity.kt
+++ app/src/main/java/com/example/cardgameapp/GameActivity.kt
@@ -1,12 +1,15 @@
 package com.example.cardgameapp
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
 
     }
 }
