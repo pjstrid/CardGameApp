1a8204ef54cb25c1e4db70a758380bc0ce772b1f
jonathan strid <pjstrid@outlook.com>
2025-11-19 18:53:39 +0000

    added buttons in activity_game

--- app/src/main/java/com/example/cardgameapp/GameActivity.kt
+++ app/src/main/java/com/example/cardgameapp/GameActivity.kt
@@ -18,17 +18,11 @@ class GameActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-
-        deckOfCards = mutableListOf(
-            "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13",
-            "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13",
-            "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13",
-            "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13"
-        )
-
-        deckOfCards.shuffle()
+        createShuffledDeck()
 
         binding.picDrawPileTopCard.setImageResource(R.drawable.backside_card)
+        binding.picBottomOfDrawPile.setImageResource(R.drawable.backside_card)
+
 
         binding.drawPile.setOnClickListener {
 
@@ -61,6 +55,17 @@ class GameActivity : AppCompatActivity() {
 
 
     }
+
+    private fun createShuffledDeck() {
+        deckOfCards = mutableListOf(
+            "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13",
+            "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13",
+            "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13",
+            "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13"
+        )
+        deckOfCards.shuffle()
+    }
+
     private fun showCard(card : String) {
         val i = when (card) {
             "h1" -> R.drawable.hearts_1
